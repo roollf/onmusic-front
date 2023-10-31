3c641834c4b5474e01be86388b223ca29a0a35b4
Rolf Matela <rolffreitas@yahoo.com.br>
2023-10-31 11:57:43 -0300

    fix: alterado temporariamente header para receber Login

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,20 @@
 import styles from './header.module.css';
+import Link from 'next/link';
 import { Search } from '@/components';
 import { HeaderList, HeaderListItem, HeaderBrand, HeaderTagLine } from './headerComponents';
 
 export default function Header({ brand, menuItems, tagLine }) {
     const menuItemsArray = menuItems.split(',').map(item => item.trim());
-    
+
     return (
         <nav className={`navbar navbar-expand-lg ${styles["navbar-header"]} ${styles["navbar-header-border"]}`}>
             <div className="container-fluid">
                 <HeaderBrand contentLink={"/"} link={brand} />
                 <div className={`collapse navbar-collapse ${styles["navbar-header-content"]}`}>
                     <HeaderList>
+                        <li className="nav-item">
+                            <Link className="nav-link" href="/users/signin">Login</Link>
+                        </li>
                         {menuItemsArray.map((item, index) => (
                             <HeaderListItem key={index} contentLink={`/${item.toLowerCase()}`} link={item} />
                         ))}
