a02ebd55cbbc55a368d956768fcc43227ed6ef12
Rolf Matela <rolffreitas@yahoo.com.br>
2023-12-06 11:43:55 -0300

    refactor: atualiza código do componente Header
    alteração e atualização do código para identificar usuário logado, utilizando o hook UserContext

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,14 @@
+'use client';
 import styles from './header.module.css';
 import Link from 'next/link';
 import { Search } from '@/components';
 import { HeaderList, HeaderListItem, HeaderBrand, HeaderTagLine } from './headerComponents';
+import { UserContext } from '../hooks/userContext';
+import { useContext } from 'react';
 
 export default function Header({ brand, menuItems, tagLine }) {
     const menuItemsArray = menuItems.split(',').map(item => item.trim());
+    const { autenticado, usuarioLogado, logout } = useContext(UserContext);
 
     return (
         <nav className={`navbar navbar-expand-lg ${styles["navbar-header"]} ${styles["navbar-header-border"]}`}>
@@ -12,12 +16,28 @@ export default function Header({ brand, menuItems, tagLine }) {
                 <HeaderBrand contentLink={"/"} link={brand} />
                 <div className={`collapse navbar-collapse ${styles["navbar-header-content"]}`}>
                     <HeaderList>
-                        <li className="nav-item">
-                            <Link className="nav-link" href="/users/signin">Login</Link>
-                        </li>
-                        {menuItemsArray.map((item, index) => (
-                            <HeaderListItem key={index} contentLink={`/${item.toLowerCase()}`} link={item} />
-                        ))}
+                        {autenticado ? (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" href={`/users/profile/${usuarioLogado._id}`}>Profile</Link>
+                                </li>
+                                {menuItemsArray.map((item, index) => (
+                                    <HeaderListItem key={index} contentLink={`/${item.toLowerCase()}`} link={item} />
+                                ))}
+                                <li className="nav-item">
+                                    <button variant="link" onClick={logout} className={`nav-link ${styles["navbar-header-button"]}`}>Logout</button>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" href="/users/signin">Login</Link>
+                                </li>
+                                {menuItemsArray.map((item, index) => (
+                                    <HeaderListItem key={index} contentLink={`/${item.toLowerCase()}`} link={item} />
+                                ))}
+                            </>
+                        )}
                     </HeaderList>
                     <Search />
                     <HeaderTagLine content={tagLine} />
