e0fea4caec2dbe944247f08efb6d40fb5283dc7b
Rolf Matela <rolffreitas@yahoo.com.br>
2023-12-06 11:41:33 -0300

    refactor: retira componente SongDropDown da rota CreatePlaylist
    retirada do componente da rota por questões de performance

--- src/components/song/songdropdown.js
+++ src/components/song/songdropdown.js
@@ -1,12 +1,12 @@
 export default function SongDropdown({ songs, selectedSong, setSelectedSong }) {
     return (
-        <select className="form-control" value={selectedSong} onChange={e => setSelectedSong(e.target.value)}>
-        <option value="" disabled hidden>Selecionar Música</option>
-        {songs.map(song => (
-            <option key={song.id} value={song.id}>
-                {song.song} {song.duration}
-            </option>
-        ))}
+        <select className="form-control" value={selectedSong} onChange={(e) => setSelectedSong(e.target.value)}>
+            <option value="" disabled hidden>Selecionar Música</option>
+            {songs.map(({ _id, song, duration }) => (
+                <option key={_id} value={_id}>
+                    {song} {duration}
+                </option>
+            ))}
         </select>
     );
 }
